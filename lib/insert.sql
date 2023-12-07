8a2efcc28ada1f5d9e419562a56709a2aca14524
VERA-OBIERO <vera.obiero@gmail.com>
2023-12-07 22:58:26 +0300

    bears done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 14, 'M', 'black', 'agitated', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 8, 'F', 'yellow', 'chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'orange', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 21, 'M', 'green', 'angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 18, 'F', 'white', 'lazy', 0);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES ( 37, 'M', 'grey', 'mysterious', 1);
